29679b5bea528576a70b3509615be7b6d9f13237
Dani <73230671+dasensiobit67@users.noreply.github.com>
2021-04-12 13:09:23 +0200

    First Commit

--- JocdeDausN1/app.js
+++ JocdeDausN1/app.js
@@ -1,1 +1,19 @@
+// Require packages and set the port
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const routes = require('./controllers/routes')
 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+routes(app);
+
+// Start the server
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+
+    console.log(`Server listening on port ${server.address().port}`);
+});
