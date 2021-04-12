fcce6b82e218421250b033c07965efa69a64229e
Dani <73230671+dasensiobit67@users.noreply.github.com>
2021-04-12 13:34:59 +0200

    Add files via upload

--- JocdeDausN1/scripts/jocdaus.sql
+++ JocdeDausN1/scripts/jocdaus.sql
@@ -1,1 +1,20 @@
+CREATE DATABASE IF NOT EXISTS jocdaus CHARACTER SET utf8;
+USE  jocdaus;
 
+CREATE TABLE  user (
+ iduser INT NOT NULL AUTO_INCREMENT,
+ username VARCHAR(45) NOT NULL,
+ datareg VARCHAR(10) NOT NULL,
+ PRIMARY KEY (iduser),
+ UNIQUE INDEX (username)
+ );
+
+CREATE TABLE  tirada (
+ idtirada  INT NOT NULL AUTO_INCREMENT,
+ dau1  INT NOT NULL,
+ dau2  INT NOT NULL,
+ winorlose BOOLEAN,
+ iduser INT NOT NULL,
+ PRIMARY KEY ( idtirada ),
+ FOREIGN KEY (iduser) REFERENCES  user (iduser)
+ );
