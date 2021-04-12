1a2e5893745a90d6cdf71071aa9f1737eb28e5c6
Dani <73230671+dasensiobit67@users.noreply.github.com>
2021-04-12 13:27:32 +0200

    Add files via upload

--- JocdeDausN1/config/db.js
+++ JocdeDausN1/config/db.js
@@ -1,1 +1,24 @@
+const mysql = require('mysql');
 
+// Set database connection credentials
+const config = {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'jocdaus',
+};
+
+// Create a MySQL pool
+const pool = mysql.createPool(config);
+
+pool.getConnection(function (err, connection) {
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("Conectado a "+config.database+"!");
+    }
+});
+
+// Export the pool
+module.exports =  pool;
