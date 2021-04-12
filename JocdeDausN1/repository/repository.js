7424d9339fb914e1faca8c5e88b16b9898476c0f
Dani <73230671+dasensiobit67@users.noreply.github.com>
2021-04-12 13:32:27 +0200

    Add files via upload

--- JocdeDausN1/repository/repository.js
+++ JocdeDausN1/repository/repository.js
@@ -1,1 +1,17 @@
+const pool = require('../config/db');
 
+let query = function (sql, values) {
+    return new Promise((resolve, reject) => {
+        pool.query(sql, values, (err, rows) => {
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve(rows);
+            }
+        })
+    })
+    
+}
+
+module.exports = query;
